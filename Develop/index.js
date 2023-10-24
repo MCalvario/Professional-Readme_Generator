94e6496c13e700116c907e6341d40af804f86d13
MaribelC <Maribel_Calvario@yahoo.com>
2023-10-24 17:35:48 -0500

    added markdown

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require(Readme-Generator/generateMarkdown.js)
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 inquirer
@@ -51,9 +52,9 @@ return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 // TODO: Create a function to initialize app
 function init() {
-inquirer.prompt(questions).then((responses) => {
+inquirer.prompt(questions).then((data) => {
     console.log("creating professional readme.md file");
-    writeToFile("README.md", generateMarkdown({responses}));
+    writeToFile("./README.md", generateMarkdown({data}));
 });
 }
 
